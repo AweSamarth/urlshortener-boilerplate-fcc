15741c89c387ff846223cdc113ded2a3db52bdbf
Samarth Saxena <samarthsaxena1672003@gmail.com>
2023-01-13 21:24:00 +0530

    some commit

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const bodyParse = require("body-parser")
+const bodyParser = require("body-parser")
 
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port =  3000;
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
 
 app.use(cors());
 
