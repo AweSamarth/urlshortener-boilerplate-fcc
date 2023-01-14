94a3155694517cffb899bfda408aa1af6a721e60
Samarth Saxena <samarthsaxena1672003@gmail.com>
2023-01-14 17:40:58 +0530

    done

--- index.js
+++ index.js
@@ -1,63 +1,63 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
-const { dnsEncode } = require('ethers/lib/utils');
-const dns = require('node:dns');
+const { dnsEncode } = require("ethers/lib/utils");
 
-var array =[]
-var controller
+var array = [];
+let regex =/(?<=(http|https|ftp):\/\/)[^\/]+/
+
 
 // Basic Configuration
-const port =  3000;
-app.use(bodyParser.urlencoded({extended:false}))
+const port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.get("/api/shorturl/:shortened", function(req, res){
-  let shorturl = req.params.shortened
-  console.log(shorturl)
-  if(shorturl.startsWith("https://")){
-    res.redirect(array[shorturl])
-  }
-  else{
-    res.redirect("https://"+array[shorturl])
-  }
-})
-app.post("/api/shorturl", function(req, res, next){
-  // console.log("this worked")
-  // console.log(req.body.url)
-  dns.lookup(req.body.url,(err,address, family)=>{
-    if(err||req.body.url==""){
-      res.json({error:"Invalid URL"})
-    }
-    else{
-      controller = req.body.url
-      console.log(address, family)
-      next()
-    }
-  })
-}, 
-function(req, res){
-  array.push(controller)
-  console.log(array)
-  res.json({original_url:controller, short_url:array.indexOf(controller)})
+app.post(
+  "/api/shorturl/",
+  function (req, res) {
+      if (!regex.test(req.body.url)) {
+        res.json({ error: "Invalid URL" });
+      } else {
 
-})
+        
+        array.push(req.body.url);
+        console.log(array);
+        res.json({
+          original_url: req.body.url,
+          short_url: parseInt(array.indexOf(req.body.url)),
+        })
+      }
+    
+  },
+);
+app.get("/api/shorturl/:shortindex", function (req, res) {
+  let shortindex = req.params.shortindex;
 
-app.listen(port, function() {
+  console.log(array[shortindex]);
+  if (array[shortindex] != undefined) {
+    if (array[shortindex].startsWith("https://")) {
+      res.redirect(array[shortindex]);
+    } else {
+      res.redirect("https://" + array[shortindex]);
+    }
+  }
+  else res.json({this:"this"})
+});
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
